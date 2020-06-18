5539bb2dd498ab5b429adc89479f9b16eef86802
009kings <009kings@gmail.com>
2020-06-17 22:12:01 -0700

    Adding solutions

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -51,6 +51,7 @@ SELECT name FROM cities WHERE id = 211;
 -- In a city of ninety-one thousand and now, eighty five.
 SELECT name FROM cities WHERE population = 91084;
 
+
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 
