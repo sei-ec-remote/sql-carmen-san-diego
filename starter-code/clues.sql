db20f01e3788b7e28c35d43c34089c369fa565d2
Zene09 <zeneorr@gmail.com>
2022-08-17 19:31:04 -0500

    Complete - Zene O

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,18 +2,23 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+SELECT * FROM countries WHERE region = 'Southern Europe' ORDER BY population LIMIT 1; 
+-- Vatican, Holy City
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
-
+SELECT * FROM countrylanguages WHERE countrycode = 'VAT';
+-- Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+SELECT countrycode FROM countrylanguages WHERE language = 'Italian' AND percentage = 100;
+-- SMR
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
@@ -21,17 +26,30 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
-
+SELECT name FROM countries WHERE code = 'SMR';
+-- San Marino, but not?
+SELECT name FROM cities WHERE countrycode = 'SMR' AND NOT name = 'San Marino';
+-- Serravalle
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+-- SELECT name FROM cities WHERE name = 'Serra' (this was right, but only on a whim I knew it's in South America)
+SELECT name FROM cities WHERE name LIKE 'Serr%' AND NOT name = 'Serravalle';
+-- Serra
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
+
+SELECT * FROM cities WHERE name LIKE 'Serr%' AND NOT name = 'Serravalle'
+--  id  | name  | countrycode |    district    | population 
+-----+-------+-------------+----------------+------------
+--  265 | Serra | BRA         | Esp�rito Santo |     302666
+SELECT * FROM countries WHERE code = 'BRA';
+--  BRA  | Brazil | South America | South America | 8.547403e+06 |      1822 |  170115000 |           62.9 | 776739.00 | 804108.00 | Brasil    | Federal Republic | Fernando Henrique Cardoso (capital)|     211 | BR
 
 
 
@@ -51,6 +69,8 @@
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
-
-
--- She's in ____________________________!
+SELECT name FROM cities WHERE population = 91084;
+-- No cities with a population of 91085, yet
+SELECT * FROM cities WHERE name = 'Santa Monica';
+--  4060 | Santa Monica | USA         | California |      91084
+-- She's in Santa Monica, CA!
