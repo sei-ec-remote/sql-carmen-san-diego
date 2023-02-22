a731f1dc2120ab5315e0e5cf568c4ad1010e1540
Travis <travishersh0@gmail.com>
2023-02-22 18:29:34 -0500

    Clue 5 - Done

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -27,8 +27,8 @@ SELECT name FROM cities WHERE countrycode = 'SMR' AND name <> 'San Marino';
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
-
+SELECT name, countrycode FROM cities WHERE name LIKE 'Serra%';
+SELECT name FROM countries WHERE code = 'BRA';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
