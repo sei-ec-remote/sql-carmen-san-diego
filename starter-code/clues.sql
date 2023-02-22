dd661b7af6761f1df943fe0ff9c0ebcfef746b69
thepjos <josh.thephasdin@gmail.com>
2023-02-22 17:08:33 -0600

    thru clue 5

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,32 +1,43 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
+-- world=# SELECT * FROM countries WHERE region = 'Southern Europe' ORDER BY population LIMIT 1
+-- answer VAT
 
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
+-- world=# SELECT * FROM countrylanguages WHERE countrycode = 'VAT';
+-- answer italian
 
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
-
+-- SELECT * FROM countrylanguages WHERE language = 'Italian' AND isofficial = 't';
+-- world=# SELECT continent, code, region FROM countries WHERE code IN ('ITA', 'SMR', 'VAT', 'CHE');
+-- ITA OR SMR
+-- world=# SELECT name, code  FROM countries WHERE code = 'SMR';
+-- world=# SELECT * FROM cities WHERE countrycode = 'SMR';
+-- figured out, its san marino
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
-
+-- world=# SELECT * FROM cities WHERE countrycode = 'SMR';
+-- Serravalle
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+-- world=# SELECT continent, countries.name, cities.name FROM countries JOIN cities ON countries.code = cities.countrycode WHERE countries.continent = 'South America' AND cities.name LIKE 'Ser%'; 
+-- going with SERRA but could be SERTAOZINHO
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
