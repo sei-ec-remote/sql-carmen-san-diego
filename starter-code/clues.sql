c3235d25793121dae61f03c42bf9259e749b4781
Travis <travishersh0@gmail.com>
2023-02-22 18:32:51 -0500

    Clue 6 - Done

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -33,8 +33,8 @@ SELECT name FROM countries WHERE code = 'BRA';
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
-
-
+SELECT capital FROM countries WHERE code = 'BRA';
+SELECT name FROM cities WHERE id = 211;
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
