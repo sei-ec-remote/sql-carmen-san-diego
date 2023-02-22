cd6c539d30e4d0892319ce612e9fa3eb23fa0ed5
KenGibb <kenjibbo@gmail.com>
2023-02-22 17:26:06 -0600

    Clue 3

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -22,11 +22,14 @@
 
 -- SELECT * FROM countrylanguages WHERE language = 'Italian' and percentage = 100;
 
+-- SELECT name FROM countries WHERE code = 'SMR';
+
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
+
 
 
 
