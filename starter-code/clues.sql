715cbcdff26cebe7d8e1ec0e385a4aaeb97467bd
KenGibb <kenjibbo@gmail.com>
2023-02-22 17:34:48 -0600

    Clue 4

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -30,8 +30,7 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
-
-
+-- SELECT name, countrycode FROM cities WHERE countrycode = 'SMR';
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
