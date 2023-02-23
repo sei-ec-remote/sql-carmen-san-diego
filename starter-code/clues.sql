d8cbb000bd743698f5791c1f1e9aafdcf3867c37
nocnoc_e <dhgustmd25@hotmail.com>
2023-02-22 22:52:00 -0500

    clue 5 done

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -52,19 +52,30 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
- SELECT name FROM cities
- WHERE countrycode = 'SMR' AND name != 'San Marino';
+--  SELECT name FROM cities
+--  WHERE countrycode = 'SMR' AND name != 'San Marino';
 
-world=# \i clues.sql
-    name    
-------------
- Serravalle
-(1 row)
+-- world=# \i clues.sql
+--     name    
+-- ------------
+--  Serravalle
+-- (1 row)
+
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+
+SELECT name, countrycode FROM cities
+WHERE name LIKE 'Serra%' AND name != 'Serravalle';
+
+world=# \i clues.sql
+ name  | countrycode 
+-------+-------------
+ Serra | BRA
+(1 row)
+
 
 
 
