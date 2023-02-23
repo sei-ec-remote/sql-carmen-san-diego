070e8e50acdadfa98f7ffa4797ab43604db5b566
bruceabrigo <bruceabrigo@outlook.com>
2023-02-22 22:48:43 -0600

    We found her in the USA! Santa Monica, California

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -27,8 +27,8 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
-SELECT * FROM cities
-WHERE countrycode LIKE 'SMR'
+-- SELECT * FROM cities
+-- WHERE countrycode LIKE 'SMR'
 -- ITA
 -- SMR
 -- VAT
@@ -40,13 +40,22 @@ WHERE countrycode LIKE 'SMR'
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-
+-- SELECT * FROM cities
+-- WHERE name LIKE 'San%'
+-- Santa Maria
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+-- SELECT * FROM countries
+-- WHERE code LIKE 'BRA'
+-- capital 211
 
+-- SELECT * FROM cities
+-- WHERE countrycode LIKE 'BRA'
+
+-- Capital = 211 / Brasilia 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -61,9 +70,12 @@ WHERE countrycode LIKE 'SMR'
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
 
+-- SELECT name, population FROM cities
+-- WHERE population BETWEEN 91000 AND 91100
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+SELECT * FROM cities
+WHERE name LIKE 'Santa Monica'
 
-
--- She's in ____________________________!
+-- She's in Santa Monica, California, USA!
